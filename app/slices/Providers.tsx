5e28dfab39ff702c9c7e08ba5a795cfb5fb54e4b
Siddharth Soni <soni28siddh@gmail.com>
2023-09-19 13:01:06 +0530

    SS: basic style implementation

--- app/slices/Providers.tsx
+++ app/slices/Providers.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from "react";
 import { store } from "@/slices";
 import { Layout } from "antd";
 import { Provider } from "react-redux";
+import Style from "@/style/Layout/index.module.scss";
 const { Header, Footer, Content } = Layout;
 interface ProviderType {
   children: ReactNode;
@@ -10,7 +11,7 @@ interface ProviderType {
 const Providers: React.FC<ProviderType> = ({ children }) => {
   return (
     <Provider store={store}>
-      <Layout>
+      <Layout className={Style.layout}>
         <Header>Header</Header>
         <Content>{children}</Content>
         <Footer>Footer</Footer>
