dd3e98f0bdd8604862d209d4e0a2b3c535fb3afa
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-21 18:24:31 +0530

    JG: custom bike data

---
+++ Mock_Data/bike.js
@@ -0,0 +1,151 @@
+export const Bike_Data = [
+    {
+        name: 'Bajaj CT 100',
+        tips: 58,
+        pickup: false,
+        hubs: [
+            "3rd Stage, Banashankari",
+            "PES University, Banashankari",
+            "Sector 1, HSR Layout",
+            "Bharat Nagar"
+        ],
+        price: 499,
+        limit: 75,
+        deposit: 2000,
+        make_year: 2021,
+        img: '',
+        duration: [
+            {
+                name: "Daily",
+                price: 359,
+                limit: 100
+            },
+            {
+                name: "7 days",
+                price: 1759,
+                limit: 700
+            },
+            {
+                name: "15 days",
+                price: 3000,
+                limit: 1500
+            },
+            {
+                name: "Monthly",
+                price: 5991,
+                limit: 2000
+            },
+        ]
+    },
+    {
+        name: 'Bajaj CT 100',
+        tips: 58,
+        pickup: true,
+        hubs: [
+            "3rd Stage, Banashankari"
+        ],
+        price: 499,
+        limit: 75,
+        deposit: 2000,
+        make_year: 2021,
+        img: '',
+        duration: [
+            {
+                name: "Daily",
+                price: 359,
+                limit: 100
+            },
+            {
+                name: "7 days",
+                price: 1759,
+                limit: 700
+            },
+            {
+                name: "15 days",
+                price: 3000,
+                limit: 1500
+            },
+            {
+                name: "Monthly",
+                price: 5991,
+                limit: 2000
+            },
+        ]
+    },
+    {
+        name: 'Bajaj CT 100',
+        tips: 58,
+        pickup: true,
+        hubs: [
+            "3rd Stage, Banashankari",
+            "PES University, Banashankari",
+            "Sector 1, HSR Layout",
+            "Bharat Nagar"
+        ],
+        price: 499,
+        limit: 75,
+        deposit: 2000,
+        make_year: 2021,
+        img: '',
+        duration: [
+            {
+                name: "Daily",
+                price: 359,
+                limit: 100
+            },
+            {
+                name: "7 days",
+                price: 1759,
+                limit: 700
+            },
+            {
+                name: "15 days",
+                price: 3000,
+                limit: 1500
+            },
+            {
+                name: "Monthly",
+                price: 5991,
+                limit: 2000
+            },
+        ]
+    },
+    {
+        name: 'Bajaj CT 100',
+        tips: 58,
+        pickup: false,
+        hubs: [
+            "3rd Stage, Banashankari",
+            "PES University, Banashankari",
+            "Sector 1, HSR Layout",
+            "Bharat Nagar"
+        ],
+        price: 499,
+        limit: 75,
+        deposit: 2000,
+        make_year: 2021,
+        img: '',
+        duration: [
+            {
+                name: "Daily",
+                price: 359,
+                limit: 100
+            },
+            {
+                name: "7 days",
+                price: 1759,
+                limit: 700
+            },
+            {
+                name: "15 days",
+                price: 3000,
+                limit: 1500
+            },
+            {
+                name: "Monthly",
+                price: 5991,
+                limit: 2000
+            },
+        ]
+    },
+]
