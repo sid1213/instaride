2c742347f7ea83cef369c6bcc6724973a3d03b1e
Siddharth Soni <soni28siddh@gmail.com>
2023-09-22 20:38:26 +0530

    SS: New component for vehicle card

--- components/Vehicle/VehicleSingle/VehicleCard/index.tsx
+++ components/Vehicle/VehicleSingle/VehicleCard/index.tsx
@@ -1,7 +1,64 @@
+"use client";
+import { Col, Row, Tag, Typography } from "antd";
 import React from "react";
+import Style from "./index.module.scss";
+import Image from "next/image";
+import FeatureCard from "@/components/Ui/FeatureCard";
+import { Fa42Group } from "react-icons/fa6";
 
-const VehicleCard = () => {
-  return <div>index</div>;
+const { Text } = Typography;
+
+const VehicleCard: React.FC = () => {
+  return (
+    <div className={Style.card}>
+      <Row gutter={10} justify={"center"} align={"middle"}>
+        <Col span={24}>
+          <Tag color="green">5 Bikes available</Tag>
+        </Col>
+        <Col span={24}>
+          <div className={Style.ImgMain}>
+            <div>
+              <Image
+                src="/demo/Suzuki-Access-125.png"
+                width={400}
+                height={260}
+                objectFit="contain"
+                alt="demo"
+              />
+            </div>
+            <Text>*Images are for representation purposes only.</Text>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <Row gutter={25}>
+              <Col>
+                <FeatureCard
+                  icon={<Fa42Group />}
+                  title="kms 1000"
+                  subTitle="6000 kms"
+                />
+              </Col>
+              <Col>
+                <FeatureCard
+                  icon={<Fa42Group />}
+                  title="kms 1000"
+                  subTitle="6000 kms"
+                />
+              </Col>
+              <Col>
+                <FeatureCard
+                  icon={<Fa42Group />}
+                  title="kms 1000"
+                  subTitle="6000 kms"
+                />
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default VehicleCard;
