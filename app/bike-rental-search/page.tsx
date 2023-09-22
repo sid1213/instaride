1072702221eab7b8ba17b9dd7e6c47f216d0544d
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-22 12:29:08 +0530

    JG: remove the style

--- app/bike-rental-search/page.tsx
+++ app/bike-rental-search/page.tsx
@@ -2,36 +2,21 @@
 import React from "react";
 import Style from "./style.module.scss";
 import { Layout } from "antd";
-import FilterCard from "@/components/FilterCard";
+import FilterCard from "@/components/vehicles/VehicleCard";
 import { Bike_Data } from "../../Mock_Data/bike";
 
 const { Header, Sider, Content } = Layout;
 
-const headerStyle: React.CSSProperties = {
-  textAlign: "center",
-  color: "#fff",
-  height: 64,
-  paddingInline: 50,
-  lineHeight: "64px",
-  backgroundColor: "#7dbcea",
-};
-
-const siderStyle: React.CSSProperties = {
-  textAlign: "center",
-  lineHeight: "120px",
-  color: "#fff",
-  backgroundColor: "#3ba0e9",
-};
 function BikeRentalSearch() {
   return (
     <Layout className={Style.bike_rental_search}>
-      <Header style={headerStyle}>Header</Header>
+      <Header></Header>
       <Layout hasSider>
-        <Sider style={siderStyle}>Sider</Sider>
+        <Sider></Sider>
         <Content>
           {Bike_Data.length > 0 &&
             Bike_Data?.map((item) => {
-              return <FilterCard data={item} />;
+              return <FilterCard data={item} key={item.id} />;
             })}
         </Content>
       </Layout>
