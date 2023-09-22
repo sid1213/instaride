02bd742a3b2311d8e3a7fb652b3699ebf9a18d72
Siddharth Soni <soni28siddh@gmail.com>
2023-09-22 17:28:21 +0530

    SS: Added initial setUp

--- app/(city)/[cityname]/[vehiclename]/page.tsx
+++ app/(city)/[cityname]/[vehiclename]/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Style from "./index.module.scss";
+import { Col, Row } from "antd";
 
 const VehicleName = () => {
-  return <div>slpender</div>;
+  return (
+    <section className={Style.container}>
+      <Row className={Style.main}>
+        <Col span={12} className={Style.left}>
+          col-8
+        </Col>
+        <Col span={12} className={Style.right}>
+          col-8
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default VehicleName;
---
+++ components/Vehicle/VehicleSingle/VehicleCard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VehicleCard = () => {
+  return <div>index</div>;
+};
+
+export default VehicleCard;
