7ba31d0c6f1b946f13f4a446323e99c21e80ae07
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-26 18:55:06 +0530

    JG: set active state

--- app/(auth)/profile/page.tsx
+++ app/(auth)/profile/page.tsx
@@ -1,19 +1,21 @@
 "use client";
-import React from "react";
-import { Layout, Space } from "antd";
+import React, { useState } from "react";
+import { Layout } from "antd";
 import Style from "./style.module.scss";
 import ProfileSidebar from "@/components/Profile/ProfileSidebar";
 import ProfileContent from "@/components/Profile/ProfileContent";
 const { Sider, Content } = Layout;
 
 function page() {
+  const [active, setActive] = useState<string>("profile");
+
   return (
     <Layout hasSider className={Style.profile_container}>
       <Sider className="profile_sidebar_container">
-        <ProfileSidebar />
+        <ProfileSidebar setActive={setActive} />
       </Sider>
       <Content className="profile_content_container">
-        <ProfileContent />
+        <ProfileContent active={active} />
       </Content>
     </Layout>
   );
