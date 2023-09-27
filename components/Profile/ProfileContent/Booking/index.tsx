f0a6b5b71f0c1d5c7d19779052e3781884bddac0
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-27 15:53:15 +0530

    JG: add left arrow with functionality

--- components/Profile/ProfileContent/Booking/index.tsx
+++ components/Profile/ProfileContent/Booking/index.tsx
@@ -4,11 +4,15 @@ import type { TabsProps } from "antd";
 import Style from "./style.module.scss";
 import { Typography } from "antd";
 import NotFound from "./NotFound";
+import { AiOutlineLeft } from "react-icons/ai";
 
 const { Title, Paragraph } = Typography;
 const onChange = (key: string) => {
   console.log(key);
 };
+interface PropsType {
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 const items: TabsProps["items"] = [
   {
@@ -27,12 +31,24 @@ const items: TabsProps["items"] = [
     children: <NotFound />,
   },
 ];
-function Booking() {
+function Booking({ setVisible }: PropsType) {
   return (
     <section className={Style.booking}>
       <div className="header">
-        <Title level={5}>Bookings</Title>
-        <Paragraph>Manage your bookings</Paragraph>
+        <div>
+          <span
+            className="left_arrow"
+            onClick={() => {
+              setVisible(false);
+            }}
+          >
+            <AiOutlineLeft />
+          </span>
+        </div>
+        <div>
+          <Title level={5}>Bookings</Title>
+          <Paragraph>Manage your bookings</Paragraph>
+        </div>
       </div>
 
       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
