456c67ba3daf5b3863b1ed60f0a3f0c6f93bea96
Siddharth Soni <soni28siddh@gmail.com>
2023-09-27 17:00:04 +0530

    SS: Added type for feature card of single vehicle page

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface ThingsToRemember {
+  icon:
+    | "SecurityDeposit"
+    | "LocationTimings"
+    | "DistanceLimit"
+    | "ExcessCharge"
+    | "LatePenalty"
+    | "SpeedLimit";
+  description: string;
+  subtitle: string;
+}
