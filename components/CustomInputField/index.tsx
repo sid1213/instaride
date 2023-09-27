8219e7ed418eeadb26d4cf1429fe41a544ecd3ff
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-27 12:47:56 +0530

    JG: set state for change the value

--- components/CustomInputField/index.tsx
+++ components/CustomInputField/index.tsx
@@ -4,17 +4,26 @@ interface PropsType {
   labelName: string;
   inputType: string;
   Width: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const CustomInputField = ({
   labelName,
   inputType,
   Width,
+  value,
+  setValue,
 }: PropsType) => {
+  const handleChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setValue(e.target.value);
+  };
   return (
     <div className={Style.custom_input_field} style={{ width: Width }}>
       <label>{labelName}</label>
-      <Input type={inputType} />
+      <Input type={inputType} value={value} onChange={(e) => handleChange(e)} />
     </div>
   );
 };
