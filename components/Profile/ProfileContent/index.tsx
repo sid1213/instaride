afd5e9a77b840388801d524dc41376266287b833
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-27 15:52:21 +0530

    JG: pass state as props

--- components/Profile/ProfileContent/index.tsx
+++ components/Profile/ProfileContent/index.tsx
@@ -3,18 +3,19 @@ import Style from "./style.module.scss";
 import ProfileEditSection from "./ProfileEditSection";
 import Booking from "./Booking";
 import GoCoins from "./GoCoins";
+import classNames from "classnames";
 
 interface PropsType {
   active: string;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
-function ProfileContent({ active }: PropsType) {
+function ProfileContent({ active, setVisible }: PropsType) {
   return (
-    <div className={Style.profile_content}>
-      {active === "profile" && <ProfileEditSection />}
-      {active === "bookings" && <Booking />}
-      {active === "goCoins" && <GoCoins />}
+    <div className={classNames(Style.profile_content)}>
+      {active === "profile" && <ProfileEditSection setVisible={setVisible} />}
+      {active === "bookings" && <Booking setVisible={setVisible} />}
+      {active === "goCoins" && <GoCoins setVisible={setVisible} />}
     </div>
   );
 }
-
 export default ProfileContent;
