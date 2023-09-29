91642506f257588e5842948cf708ae0cff210cc8
Jagrati Gupta <123jagratigupta@gmail.com>
2023-09-29 17:21:38 +0530

    JG: remove layout

--- components/Profile/ProfileContent/GoCoins/index.tsx
+++ components/Profile/ProfileContent/GoCoins/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Style from "./style.module.scss";
-import { Typography } from "antd";
+import { Button, Form, Input, Typography } from "antd";
 import { AiOutlineLeft } from "react-icons/ai";
 
 const { Title, Paragraph } = Typography;
@@ -9,25 +9,7 @@ interface PropsType {
 }
 
 function GoCoins({ setVisible }: PropsType) {
-  return (
-    <section className={Style.goCoins}>
-      <div className="header">
-        <div>
-          <span
-            className="left_arrow"
-            onClick={() => {
-              setVisible(false);
-            }}
-          >
-            <AiOutlineLeft />
-          </span>
-        </div>
-        <div>
-          <Title level={5}>Coming Soon...</Title>
-        </div>
-      </div>
-    </section>
-  );
+  return <h1>Coming soon</h1>;
 }
 
 export default GoCoins;
