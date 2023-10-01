6ac0a62073904724df342985e6d1703d06c79cca
Siddharth Soni <soni28siddh@gmail.com>
2023-10-01 19:26:53 +0530

    SS: Fixed Layout

--- app/(city)/[cityname]/page.tsx
+++ app/(city)/[cityname]/page.tsx
@@ -1,17 +1,16 @@
-"use client";
 import React from "react";
 import Style from "./style.module.scss";
 import { Layout } from "antd";
-
-const { Header, Sider, Content } = Layout;
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
 
 const City = () => {
   return (
     <Layout className={Style.city}>
-      <Header>Header</Header>
+      <Header className={Style.header}>Header</Header>
       <Layout hasSider>
-        <Sider className={Style.filter_sider}>Sider</Sider>
-        <Content className={Style.card_content}>Card</Content>
+        <Sider className={Style.sider}>Sider</Sider>
+        <Content className={Style.content}>Card</Content>
       </Layout>
     </Layout>
   );
