93345c15946c7a8901ff7e60628ba9e3f7f2ed39
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-01 21:02:54 +0530

    JG: add vehicle card

--- app/(city)/[cityname]/page.tsx
+++ app/(city)/[cityname]/page.tsx
@@ -3,6 +3,8 @@ import Style from "./style.module.scss";
 import { Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+import { Bike_Data } from "Mock_Data/bike";
+import VehicleCard from "@/components/Vehicle/VehicleCard";
 
 const City = () => {
   return (
@@ -10,7 +12,12 @@ const City = () => {
       <Header className={Style.header}>Header</Header>
       <Layout hasSider>
         <Sider className={Style.sider}>Sider</Sider>
-        <Content className={Style.content}>Card</Content>
+        <Content className={Style.content}>
+          {Bike_Data.length > 0 &&
+            Bike_Data?.map((item) => {
+              return <VehicleCard data={item} key={item.id} />;
+            })}
+        </Content>
       </Layout>
     </Layout>
   );
