520d45584e161815e93b06a777ca25e4fb41ca2c
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-01 18:44:32 +0530

    JG add layout

--- app/(city)/[cityname]/page.tsx
+++ app/(city)/[cityname]/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import React from "react";
+import Style from "./style.module.scss";
+import { Layout } from "antd";
 
-const City = () => {
-  return <>City</>;
-};
+const { Header, Sider, Content } = Layout;
+
+function City() {
+  return (
+    <Layout className={Style.city}>
+      <Header></Header>
+      <Layout hasSider>
+        <Sider></Sider>
+        <Content>Card</Content>
+      </Layout>
+    </Layout>
+  );
+}
 
 export default City;
