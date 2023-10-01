5f082b077baf2c627959e48d17fba1bd028972ff
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-01 18:20:31 +0530

    JG: add from instead of div

--- components/CustomInputField/index.tsx
+++ components/CustomInputField/index.tsx
@@ -1,5 +1,5 @@
 import Style from "./style.module.scss";
-import { Input } from "antd";
+import { Form, Input } from "antd";
 interface PropsType {
   labelName: string;
   inputType: string;
@@ -15,15 +15,54 @@ export const CustomInputField = ({
   value,
   setValue,
 }: PropsType) => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
   const handleChange = (e: {
     target: { value: React.SetStateAction<string> };
   }) => {
     setValue(e.target.value);
   };
   return (
-    <div className={Style.custom_input_field} style={{ width: Width }}>
-      <label>{labelName}</label>
-      <Input type={inputType} value={value} onChange={(e) => handleChange(e)} />
-    </div>
+    <Form
+      name="basic"
+      layout="vertical"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      className={Style.custom_form}
+    >
+      <Form.Item
+        label={labelName}
+        name={labelName}
+        rules={[{ message: "Enter firstName!" }]}
+        className={Style.custom_item}
+      >
+        <Input
+          className={Style.custom_input}
+          type={inputType}
+          value={value}
+          onChange={(e) => handleChange(e)}
+        />
+      </Form.Item>
+      <Form.Item
+        label={labelName}
+        name={labelName}
+        rules={[{ message: "Enter firstName!" }]}
+        className={Style.custom_item}
+      >
+        <Input
+          className={Style.custom_input}
+          type={inputType}
+          value={value}
+          onChange={(e) => handleChange(e)}
+        />
+      </Form.Item>
+    </Form>
   );
 };
