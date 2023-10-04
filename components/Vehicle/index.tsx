add66c4102987dad9c5a155801c72926ded6be13
Siddharth Soni <soni28siddh@gmail.com>
2023-10-04 12:14:10 +0530

    SS: Added view for mobile filter

---
+++ components/Vehicle/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MobileFilter from "./MobileFilter";
+// import Style from "./index.module.scss";
+const Vehicle = () => {
+  return (
+    <section>
+      <MobileFilter />
+    </section>
+  );
+};
+
+export default Vehicle;
