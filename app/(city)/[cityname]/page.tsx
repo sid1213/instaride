a6d1fe3b468204374136bd1616af268db6513037
Siddharth Soni <soni28siddh@gmail.com>
2023-10-04 09:31:22 +0530

    SS: Fixed sider style and Layout

--- app/(city)/[cityname]/page.tsx
+++ app/(city)/[cityname]/page.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import Style from "./style.module.scss";
 import { Layout } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
+import { Content } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+import Filter from "@/components/Vehicle/Filter";
+import Vehicle from "@/components/Vehicle";
+import Style from "./style.module.scss";
 
 const City = () => {
   return (
     <Layout className={Style.city}>
-      <Header className={Style.header}>Header</Header>
       <Layout hasSider>
-        <Sider className={Style.sider}>Sider</Sider>
-        <Content className={Style.content}>Card</Content>
+        <Sider className={Style.sider} width={250}>
+          <Filter />
+        </Sider>
+        <Content className={Style.content}>
+          <Vehicle />
+        </Content>
       </Layout>
     </Layout>
   );
