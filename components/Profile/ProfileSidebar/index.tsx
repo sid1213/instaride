780fcc94e82457c0bbbd3023cfccee21dd3782e2
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-05 14:12:58 +0530

    JG: change state name, class name and add badge in goCoins tab

--- components/Profile/ProfileSidebar/index.tsx
+++ components/Profile/ProfileSidebar/index.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Style from "./style.module.scss";
 import Title from "antd/es/typography/Title";
-import { Button, Col, Image, Row, Tooltip } from "antd";
+import { Badge, Button, Col, Image, Row, Space, Tooltip } from "antd";
 
 // icons
 import { FaUserAlt, FaLocationArrow, FaWallet } from "react-icons/fa";
@@ -31,47 +31,46 @@ interface PropsType {
   visible: boolean;
 }
 function ProfileSidebar({ setActive, setVisible, visible }: PropsType) {
-  const [clickedButton, setClickedButton] = useState<number>(0);
-  const toggleTabs = (buttonId: number) => setClickedButton(buttonId);
+  const [tabIndex, setTabIndex] = useState<number>(0);
+  const toggleTabs = (buttonId: number) => setTabIndex(buttonId);
 
   return (
     <Row className={Style.profile_sidebar} gutter={[0, 16]}>
       {/* profile image */}
-      <Col className={Style.profile_image_container} span={24}>
+      <Col className={Style.image_container} span={24}>
         <Image
           src="./assests/profile.gif"
           preview={false}
-          className={Style.profile_img}
+          className={Style.img}
         />
         <Title level={5}> Jagrati Gupta</Title>
       </Col>
 
       {/* Links */}
-      <Col className={Style.profile_sidebar_linklist} span={24}>
+      <Col className={Style.buttons_container} span={24}>
         {Profile_List_Data.map((data, i) => {
           return (
             <React.Fragment key={i}>
               {data.btn_name === "goCoins" ? (
-                <Tooltip title="Coming Soon.." className={Style.disabled_btn}>
-                  <Button
-                    key={i}
-                    block
-                    disabled={true}
-                    className={Style.profile_links}
-                  >
-                    <div>
+                <Badge
+                  count={"Coming Soon.."}
+                  className={Style.disabled_btn}
+                  color="orange"
+                >
+                  <Button key={i} block disabled={true}>
+                    <Space>
                       {data.icon}
                       {data.name}
-                    </div>
+                    </Space>
                   </Button>
-                </Tooltip>
+                </Badge>
               ) : (
                 <Button
                   key={i}
                   block
                   className={classNames(
-                    Style.profile_links,
-                    clickedButton === i ? Style.change_link : ""
+                    Style.profile_tab,
+                    tabIndex === i ? Style.change_tab : ""
                   )}
                   onClick={() => {
                     if (data.btn_name) {
