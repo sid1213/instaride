3a321ff40302c474cadd801b136f4d366ac52225
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-06 15:53:03 +0530

    JG: add fare component

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,6 +6,7 @@ import Sider from "antd/es/layout/Sider";
 import Style from "./style.module.scss";
 import Title from "antd/es/typography/Title";
 import SummaryOfVehicle from "@/components/Checkout/sider";
+import FareDetails from "@/components/Checkout/content";
 function Checkout() {
   return (
     <Layout className={Style.checkout}>
@@ -16,7 +17,9 @@ function Checkout() {
         <Sider className={Style.sider} width={800}>
           <SummaryOfVehicle />
         </Sider>
-        <Content className={Style.content}>Content</Content>
+        <Content className={Style.content}>
+          <FareDetails />
+        </Content>
       </Layout>
     </Layout>
   );
