ea5aac0b81764c6fa3223fc7fa149f18c29405bf
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-06 14:00:14 +0530

    JG: add checkout layout

---
+++ app/checkout/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// utils
+import { Layout } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import Style from "./style.module.scss";
+import Title from "antd/es/typography/Title";
+import SummaryOfVehicle from "@/components/Checkout/sider";
+function Checkout() {
+  return (
+    <Layout className={Style.checkout}>
+      <Header className={Style.header}>
+        <Title level={5}>Summary</Title>
+      </Header>
+      <Layout hasSider>
+        <Sider className={Style.sider} width={800}>
+          <SummaryOfVehicle />
+        </Sider>
+        <Content className={Style.content}>Content</Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default Checkout;
