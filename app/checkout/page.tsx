d754781aeb6e23753388b7b85c576597d9b6a99b
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-07 14:00:05 +0530

    JG: pass state for available the partial payment

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 // utils
 import { Layout } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
@@ -8,6 +9,8 @@ import Title from "antd/es/typography/Title";
 import SummaryOfVehicle from "@/components/Checkout/sider";
 import FareDetails from "@/components/Checkout/content";
 function Checkout() {
+  // set partial payment true-false
+  const [isPartial, setIsPartial] = useState<boolean>(false);
   return (
     <Layout className={Style.checkout}>
       <Header className={Style.header}>
@@ -15,10 +18,10 @@ function Checkout() {
       </Header>
       <Layout hasSider className={Style.innerlayout}>
         <Sider className={Style.sider}>
-          <SummaryOfVehicle />
+          <SummaryOfVehicle setIsPartial={setIsPartial} />
         </Sider>
         <Content className={Style.content}>
-          <FareDetails />
+          <FareDetails isPartial={isPartial} />
         </Content>
       </Layout>
     </Layout>
