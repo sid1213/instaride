5351b2f849df596583357582f245eba3fd3003d2
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-09 18:10:08 +0530

    JG: add login popup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,82 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import Style from "./style.module.scss";
+import { Select, Input, Modal, Space, Typography } from "antd";
+import Image from "next/image";
+import Title from "antd/es/typography/Title";
+import Button from "@/components/Ui/Button";
+const { Text } = Typography;
+const { Option } = Select;
 function Login() {
-  return <section className="container">Login</section>;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [items, setItems] = useState([
+    {
+      label: (
+        <Space>
+          <Image width={20} height={20} src={"/demo/india.svg"} alt="india" />
+          <p>+91</p>
+        </Space>
+      ),
+      value: "india",
+    },
+    {
+      label: (
+        <Space align="center">
+          <Image width={20} height={20} src={"/demo/india.svg"} alt="india" />
+          <p>+91</p>
+        </Space>
+      ),
+      value: "india",
+    },
+  ]);
+
+  const showLoginModel = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <section className="container">
+      <li onClick={showLoginModel}>Login</li>
+      <Modal
+        title=""
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        className={Style.login_popup}
+        centered
+      >
+        <Space align="center" direction="vertical" className={Style.flex_box}>
+          <Title level={4} className={Style.heading}>
+            Welcome to GoBikes Commuting Made Easy, Affordable and Quick
+          </Title>
+          <Space.Compact block>
+            <Select
+              style={{ width: 100 }}
+              defaultValue={items[0].label}
+              allowClear
+              options={items.map((item) => ({
+                label: item.label,
+                value: item.value,
+              }))}
+            />
+            <Input
+              type="number"
+              defaultValue="Xihu District, Hangzhou"
+              placeholder="Phone Number"
+            />
+          </Space.Compact>
+          <Button title="SEND OTP" fit={100} size="medium" />
+        </Space>
+      </Modal>
+    </section>
+  );
 }
 
 export default Login;
