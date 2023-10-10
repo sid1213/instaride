29efc6f92eeaf7b2fb253602e5667e3da017b71a
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-10 18:50:09 +0530

    JG: add new item in select box and hide the typography

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,10 +5,10 @@ import { Select, Input, Modal, Space, Typography } from "antd";
 import Image from "next/image";
 import Title from "antd/es/typography/Title";
 import Button from "@/components/Ui/Button";
-const { Text } = Typography;
-const { Option } = Select;
+// const { Text } = Typography;
+// const { Option } = Select;
 function Login() {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
   const [items, setItems] = useState([
     {
       label: (
@@ -22,14 +22,21 @@ function Login() {
     {
       label: (
         <Space align="center">
-          <Image width={20} height={20} src={"/demo/india.svg"} alt="india" />
+          <Image
+            width={20}
+            height={20}
+            src={"/demo/india.svg"}
+            alt="australia"
+          />
           <p>+91</p>
         </Space>
       ),
-      value: "india",
+      value: "australia",
     },
   ]);
 
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [isDisable, setIsDisable] = useState<boolean>(true);
   const showLoginModel = () => {
     setIsModalOpen(true);
   };
@@ -39,6 +46,14 @@ function Login() {
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+  };
+  const handleForMobile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length <= 10) {
+      setPhoneNumber(e.target.value);
+      e.target.value.length == 10 ? setIsDisable(false) : setIsDisable(true);
+    } else {
+      setIsDisable(false);
+    }
   };
 
   return (
@@ -54,12 +69,13 @@ function Login() {
       >
         <Space align="center" direction="vertical" className={Style.flex_box}>
           <Title level={4} className={Style.heading}>
-            Welcome to GoBikes Commuting Made Easy, Affordable and Quick
+            Welcome to <span>InstaRide</span> Commuting Made Easy, <br />
+            Affordable and Quick
           </Title>
           <Space.Compact block>
             <Select
               style={{ width: 100 }}
-              defaultValue={items[0].label}
+              defaultValue={items[0].value}
               allowClear
               options={items.map((item) => ({
                 label: item.label,
@@ -68,11 +84,17 @@ function Login() {
             />
             <Input
               type="number"
-              defaultValue="Xihu District, Hangzhou"
               placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => handleForMobile(e)}
             />
           </Space.Compact>
-          <Button title="SEND OTP" fit={100} size="medium" />
+          <Button
+            title="SEND OTP"
+            fit={100}
+            size="medium"
+            disabled={isDisable}
+          />
         </Space>
       </Modal>
     </section>
