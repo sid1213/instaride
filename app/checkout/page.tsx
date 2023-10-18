76bef6141d714d343df9eefe6e9a50e60c4155b1
Jagrati Gupta <123jagratigupta@gmail.com>
2023-10-18 10:51:43 +0530

    JG: change the layout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import React, { useState } from "react";
 // utils
-import { Layout } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
-import Sider from "antd/es/layout/Sider";
+import { Col, Row } from "antd";
 import Style from "./style.module.scss";
 import Title from "antd/es/typography/Title";
 import SummaryOfVehicle from "@/components/Checkout/sider";
@@ -12,19 +10,19 @@ function Checkout() {
   // set partial payment true-false
   const [isPartial, setIsPartial] = useState<boolean>(false);
   return (
-    <Layout className={Style.checkout}>
-      <Header className={Style.header}>
-        <Title level={5}>Summary</Title>
-      </Header>
-      <Layout hasSider className={Style.innerlayout}>
-        <Sider className={Style.sider}>
+    <section className={Style.container}>
+      <Row className={Style.main}>
+        <Col span={24} className={Style.header}>
+          <Title level={5}>Summary</Title>
+        </Col>
+        <Col span={24} md={24} lg={13} xl={13} className={Style.left}>
           <SummaryOfVehicle setIsPartial={setIsPartial} />
-        </Sider>
-        <Content className={Style.content}>
+        </Col>
+        <Col span={24} md={24} lg={10} xl={10} className={Style.right}>
           <FareDetails isPartial={isPartial} />
-        </Content>
-      </Layout>
-    </Layout>
+        </Col>
+      </Row>
+    </section>
   );
 }
 
