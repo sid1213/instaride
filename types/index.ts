b386ad02493777979e286937fe96b150f64ca044
Jagrati Gupta <123jagratigupta@gmail.com>
2023-11-18 12:56:44 +0530

    JG: add user props type

--- types/index.ts
+++ types/index.ts
@@ -25,3 +25,9 @@ export interface WhyChooseUs {
   subtitle: string;
   description: string;
 }
+
+export interface User {
+  name?: string | undefined | null;
+  email?: string | undefined | null;
+  image?: string | undefined | null;
+}
